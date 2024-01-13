628bdc732aebc120caffb1bfe6531824ee39f552
DataCodeLiteracy <dataliteracy@icloud.com>
2024-01-13 13:35:26 +0900

    feat: 로그인, 회원가입 페이지에서는 네비게이션 보여주지 않게 구현
    2024.01.13

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Outlet } from 'react-router-dom'
 import Nav from './components/Nav/Nav'
+import { useEffect, useState } from 'react'
 
 const App = () => {
+  const [path, setPath] = useState('')
+  const pathname = window.location.pathname
+
+  useEffect(() => {
+    setPath(pathname)
+  }, [pathname])
+
   return (
     <section className="flex flex-col justify-center w-1024 m-auto">
-      <Nav />
+      {!(path === '/login' || path === '/register') && (
+        <Nav setPath={setPath} />
+      )}
       <Outlet />
     </section>
   )
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,10 +1,19 @@
+import { useEffect } from 'react'
 import { CiShop } from 'react-icons/ci'
 import { FaCartArrowDown } from 'react-icons/fa'
 import { FaPencilAlt } from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
 
-const Nav = () => {
+const Nav = ({ setPath }: any) => {
+  const navigate = useNavigate()
+
+  const goToTheLoginPage = () => {
+    setPath('/login')
+    navigate('/login')
+  }
+
   return (
-    <section className="flex justify-between p-15 h-50 border-solid border-b border-gray-700">
+    <nav className="flex justify-between p-15 h-50 border-solid border-b border-gray-700">
       <div className="flex items-center">
         <CiShop className="text-25" />
         <span className="ml-10 text-20">Shoppy</span>
@@ -12,9 +21,14 @@ const Nav = () => {
       <div className="flex items-center justify-evenly w-1/5 ">
         <FaCartArrowDown className="text-20" />
         <FaPencilAlt className="text-20" />
-        <button className="text-20">Login</button>
+        <button
+          className="p-6 bg-red-400 text-20 text-white"
+          onClick={goToTheLoginPage}
+        >
+          Login
+        </button>
       </div>
-    </section>
+    </nav>
   )
 }
 
